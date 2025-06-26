0032d8927e43bee00910a62dc68a48a396666624
DIVYANSH1610 <142164770+DIVYANSH1610@users.noreply.github.com>
2025-06-27 01:16:58 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/todo-app">
+    <BrowserRouter basename="/Todo-app">
       <App />
     </BrowserRouter>
   </React.StrictMode>
